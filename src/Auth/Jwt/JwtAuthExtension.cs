1ec3f570e553d17cf3456dfc848a9dd50b8fe934
a1unade <tags0956@gmail.com>
2026-01-30 02:59:23 +0300

    [add] jwt auth extension

---
+++ src/Auth/Jwt/JwtAuthExtension.cs
@@ -0,0 +1,47 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Planara.Common.Auth.Jwt;
+
+public static class JwtAuthExtension
+{
+    public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddOptions<JwtOptions>()
+            .Bind(configuration.GetSection("Jwt"))
+            .ValidateDataAnnotations()
+            .Validate(o => o.SigningKey.Length >= 32, "SigningKey must be at least 32 chars")
+            .ValidateOnStart();
+        
+        services
+            .AddOptions<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme)
+            .Configure<IOptions<JwtOptions>>((options, jwtOpt) =>
+            {
+                var jwt = jwtOpt.Value;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwt.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = jwt.Audience,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.SigningKey)),
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromSeconds(30)
+                };
+            });
+        
+        services.AddAuthorization();
+        
+        return services;
+    }
+}
